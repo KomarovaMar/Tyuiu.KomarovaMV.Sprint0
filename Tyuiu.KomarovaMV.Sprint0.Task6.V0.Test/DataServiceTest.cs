c91dc12918ea1e64d08d5e879ca648795a6dbe6e
Маргарита Комарова <Tyuiu.komarovaMV@outlook.com>
2024-09-19 20:53:06 +0500

    Task6

---
+++ Tyuiu.KomarovaMV.Sprint0.Task6.V0.Test/DataServiceTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KomarovaMV.Sprint0.Task6.V0.Lib;
+
+namespace Tyuiu.KomarovaMV.Sprint0.Task6.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckAdditionArrayValid()
+        {
+            var nambers = new int [] { 1, 2, 3, 4, 5 };
+            var res = DataService.AdditionArray(nambers);
+            Assert.AreEqual(15, res);
+        }
+        public void CheckSubtractionArrayValid()
+        {
+            var nambers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.AdditionArray(nambers);
+            Assert.AreEqual(-15, res);
+        }
+        public void CheckMultiplicationArrayValid()
+        {
+            var nambers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.AdditionArray(nambers);
+            Assert.AreEqual(120, res);
+        }
+
+    }
+}
---
+++ Tyuiu.KomarovaMV.Sprint0.Task6.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KomarovaMV.Sprint0.Task6.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KomarovaMV.Sprint0.Task6.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("64ae1f0a-126e-42ac-8fe1-7db7151addf1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
